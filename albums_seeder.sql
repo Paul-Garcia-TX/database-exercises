936ca54d89943d9dbe20869c1f1e1b2b653f6fcf
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-19 14:35:36 -0500

    seeder done

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+    INSERT INTO albums (id, artist,name,release_date,sales,genre)
+VALUES (3,'Led Zeppelin','Led Zeppelin IV', 1971,30400000,'Hard Rock'),
+(4,'Michael Jackson','Bad',1987,35000000,'Pop'),
+(5,'Alanis Morissette','Jagged Little Pill',1995,32400000,'Alternative Rock'),
+(6,'Celine Dion','Falling Into You', 1996,32100000,'Pop'),
+(7,'Adele','21',2011,31000000,'Pop'),
+(8,'The Beatles','1',2000,31000000,'Rock'),
+(9,'Metallica','Metallica',1991,31000000,'Heavy Metal'),
+(10,'Celine Dion','Lets Talk About Love',1997,31000000,'Pop');
